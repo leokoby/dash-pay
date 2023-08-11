04abffd01c580d4c4c9f44ba859aaa07d991f5f2
Leonardo Rocha <leokoby1@gmail.com>
2023-08-11 23:06:55 +0100

    feature: start header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav>
+      <div>
+        <span>
+          <a href="/">DashPay</a>
+        </span>
+        <span>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Services</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </span>
+      </div>      
+    </nav>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider , createBrowserRouter } from 'react-router-dom'
 import './index.css'
+
+import Home from './pages/Home/Home.tsx'
+import Header from './components/Header/Header.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
