ee80bff88528dde28849ce6d7a28bf432af586f1
Leonardo Rocha <leokoby1@gmail.com>
2023-08-30 12:41:10 +0100

    adjusted name currency formatter function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-const currencyFormater = (value: number) => {
+const currencyFormatter = (value: number) => {
   const valueFormater = new Intl.NumberFormat('pt-pt', {
     style: "currency",
     currency: "EUR"
